df44b2c4e032dda7a51ccf1aa55b11c7918dfdb0
keithburgie <krburgie@gmail.com>
2019-06-22 15:24:55 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+
+let heyJude = function(){
+  return "Na na na na na na na, na na na na, hey Jude.".repeat(16);
+}
+
+
+
+// Create a function called prohibitedLanguage that will prevent usernames with the Strings "candycorn" and/or "brusselssprouts"
+// Note: A tool in regular expressions is the pipe character | (the pipe character that shares the backslash \ key on standard keyboards) which allows you to search for alternative words /RegExp1|RegExp2/. Instead of just searching for just one word, we can use the pipe character to search for multiple words.
+
+let prohibitedLanguage = function(string) {
+  let bannedWords = /candycorn|brusselssprouts/;
+  return string.search(bannedWords);
+}
