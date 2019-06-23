a464980172c2177fdf21c44288037e503dc27351
Your Name <you@example.com>
2019-06-23 11:15:38 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution here
+let verse = "Na na na na na na na, na na na na, hey Jude."
+function heyJude(verse){
+  return verse.repeat(16)
+}
+function prohibitedLanguage(string){
+return string.search(/candycorn|brusselssprouts/);
+
+
+}
