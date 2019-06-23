aecbb97d8a3fc1a50063d279cc3d684932372ee2
Nicholas Orochena <nicholas.orochena@gmail.com>
2019-06-23 18:06:13 -0400

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+
+const myRegExp = /brusselssprouts|candycorn/;
+
+let heyJude = function() {
+    return "Na na na na na na na, na na na na, hey Jude.".repeat(16)
+}
+
+let prohibitedLanguage = function(str) {
+    return str.search(myRegExp)
+}
+
