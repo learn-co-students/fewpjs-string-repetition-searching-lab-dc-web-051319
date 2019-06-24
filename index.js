0d43c44629f15e2a5189c8754c4e5405f7d2fd1c
Emily Bolten <emilybolten@gmail.com>
2019-06-24 15:54:05 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution here
+function heyJude() {
+    return "Na na na na na na na, na na na na, hey Jude.".repeat(16);
+};
+
+function prohibitedLanguage(name) {
+    let bannedWords = /brusselssprouts|candycorn/;
+    return name.search(bannedWords)
+};
+
