364627b37b6ff480a3599a233c963f1a1553423f
Robert Pedersen <rob@worldcrimeleague.org>
2019-06-24 00:11:21 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution here
+
+let heyJude = (verse) => {
+  return verse.repeat(16);
+}
+// "Na na na na na na na, na na na na, hey Jude.".repeat(16);
+
+let prohibitedLanguage = (string) => {
+  let bannedWords = /candycorn|brusselssprouts/;
+  return string.search(bannedWords);  
+}
