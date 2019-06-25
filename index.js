34743f0553bd5c968a84b05adc7193ea1a2e1a8e
tariqjunaid <junaidtariq_91@yahoo.com>
2019-06-25 11:50:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Code your solution here
+const heyJude = verse => verse.repeat(16)
+
+const prohibitedLanguage = string => {
+    let bannedWords = /candycorn|brusselssprouts/
+    return string.search(bannedWords)
+}
